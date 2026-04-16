d0e898fd3d61a39b87b2cc8fe3323b88c925a55f
Mark Dierolf <mdierolf@cloudquote.io>
2026-04-16 08:42:06 -0700

    Refactor `AgentLifecycleService` to rename and update `KeyedRegistry` method calls for clarity and consistency

--- AgentLifecycleService.ts
+++ AgentLifecycleService.ts
@@ -13,9 +13,9 @@ export default class AgentLifecycleService implements TokenRingService {
 
   private hooks = new KeyedRegistry<HookSubscription>();
 
-  registerHook = this.hooks.register;
-  getAllHookEntries = this.hooks.entries;
-  getAllHookNames = this.hooks.getAllItemNames;
+  registerHook = this.hooks.set;
+  getAllHookEntries = this.hooks.entriesArray;
+  getAllHookNames = this.hooks.keysArray;
 
   constructor(readonly options: ParsedLifecycleServiceConfig) {
   }
@@ -28,14 +28,14 @@ export default class AgentLifecycleService implements TokenRingService {
 
     // The enabled tools can include wildcards, so they need to be mapped to actual tool names with ensureItemNamesLike
     agent.initializeState(LifecycleState, {
-      enabledHooks: this.hooks.ensureItemNamesLike(enabledHooks),
+      enabledHooks: this.hooks.requireKeysLike(enabledHooks),
       ...config,
     });
   }
 
   addHooks(hooks: Record<string, HookSubscription>) {
     for (const hookName in hooks) {
-      this.hooks.register(hookName, hooks[hookName]);
+      this.hooks.set(hookName, hooks[hookName]);
     }
   }
 
@@ -74,7 +74,7 @@ export default class AgentLifecycleService implements TokenRingService {
 
   async executeHooks(data: Hook, agent: Agent): Promise<void> {
     for (const hookName of this.getEnabledHooks(agent)) {
-      const subscription = this.hooks.getItemByName(hookName);
+      const subscription = this.hooks.get(hookName);
       if (subscription) {
         for (const callback of subscription.callbacks) {
           if (callback.hookConstructor === data.constructor) {
